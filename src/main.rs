815ec3daeadae0aef04460901b5d12312317a2ef
antonio <47569261+antferdom@users.noreply.github.com>
2022-09-12 13:42:42 +0200

    HACKING: compiler linking reduce complexity

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,4 @@ mod generation;
 fn main() {
     println!("Hello, world!");
 
-    let mut vec: Vec<f64> = vec![1., 2., 3.];
-    let threshold: f64 = 1.5;
-
-    println!("The result is {}", generation::has_close_elements(vec, threshold));
-
 }
